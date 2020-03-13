f80d8fb9c1d75168fc1f5b16d69041c7ab405ad5
rajankhadka <rajankhadka5@gmail.com>
2020-03-13 21:22:07 +0545

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,819 @@
+// import {username} from './index';
+
+let count  =0;
+let question_number = 1;
+
+let quiz = [
+    {
+        question:"What is the scientific name of a butterfly 0 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly  1 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly  2 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly  3 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly?  4 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly?  5  ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 6  ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly?  7 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 8 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 9 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 10 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 11 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly?   12 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 13 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 14 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly? 15 ",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 16 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 17 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 18 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 19 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 20 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 21 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 22 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 23 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 24 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 25 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 26 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 27 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 28 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 29 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },{
+        question:"What is the scientific name of a butterfly 30 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly 31 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly 32 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly 33 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly 34 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+    {
+        question:"What is the scientific name of a butterfly  35 ?",
+        options:[
+            {
+                A:"Apis"
+
+            },
+            {
+                B:"Coleoptera"
+            },{
+                C:"Formicidae"
+            },{
+                D:"Rhopalocera"
+            }
+        ],
+        Answer:"Rhopalocera",
+    },
+];
+
+
+
+
+
+
+
+
+
+
+let quiz_buffer = {};
+
+let start_game = document.getElementById('start_game');
+
+let next_game = document.getElementById("next_game");
+
+next_game.style.display = 'none';
+
+let quiz_game = document.getElementById('quiz_game');
+start_game.addEventListener('click',event=>{
+
+    let restart_game = document.createElement('a');
+    restart_game.textContent = 'Restart Game';
+    restart_game.setAttribute('id','restart_game');
+    restart_game.setAttribute('href','quiz.html');
+    (quiz_game.parentNode).appendChild(restart_game);
+    restart_game.style.display = 'none';
+
+
+
+
+
+    let random= Math.floor((Math.random())* (quiz.length -1 ))+1;
+    console.log(random);
+    let h1 = document.createElement('h1');
+    h1.textContent = quiz[random]["question"];
+    h1.setAttribute( "id", "quiz_question");
+
+    quiz_game.appendChild(h1);
+    let choose = ["A",'B',"C","D"];
+    for (let i = 0;i<4;i++){
+        let button = document.createElement("button");
+        button.textContent = quiz[random]["options"][i][choose[i]];
+        quiz_game.appendChild(button);
+        // console.log(button);
+        button.setAttribute('id','button'+i);
+
+    }
+    quiz_buffer[random]  = 1;
+    start_game.style.display = 'none';
+    // next_game.style.display = 'block';
+    let a_tag = document.getElementById('a_tag');
+    a_tag.style.marginLeft = "80%";
+    // console.log(a_tag);
+    a_tag.style.marginTop = '0';
+    next_game.style.marginTop = '35%';
+
+    let option_button = document.querySelectorAll('#quiz_game button');
+
+    for(let i = 0;i<4;i++){
+        console.log(option_button[i]);
+        option_button[i].addEventListener('click',event=>{
+            // console.log(random);
+            // console.log(option_button[i].textContent);
+            // console.log(quiz[random]["Answer"]);
+            if(option_button[i].textContent === quiz[random]["Answer"]){
+                console.log("it's correct !!!");
+                option_button[i].style.backgroundColor = '#2ecc71';
+                count ++;
+                console.log(count);
+                option_button[i].disabled = true;
+                next_game.style.display = 'block';
+            }
+            else {
+
+                option_button[i].style.backgroundColor = 'red';
+                console.log("count  --->"+ count);
+                next_game.style.display = 'block';
+
+            }
+        });
+    }
+
+
+
+
+
+});
+
+
+next_game.addEventListener('click',event=>{
+    next_game.style.display = 'none';
+    let random= Math.floor((Math.random())* (quiz.length -1 ))+1;
+
+    while(true){
+       if  (random in quiz_buffer){
+           random= Math.floor((Math.random())* (quiz.length -1 ))+1;
+
+       }
+       else{
+           quiz_buffer[random] = 1;
+           break;
+       }
+    }
+
+    let quiz_question = document.getElementById('quiz_question');
+    quiz_game.removeChild(quiz_question);
+    question_number++;
+
+    if (question_number > 10){
+        next_game.style.display = 'none';
+
+        let finish_game = document.createElement('h1');
+        finish_game.textContent = 'Game Over';
+        finish_game.setAttribute('id','finish_game');
+        // finish_game.setAttribute('href','leaderboard.html');
+        // quiz_game.appendChild(finish_game);
+
+
+        (quiz_game.parentNode).appendChild(finish_game);
+        // finish_game.addEventListener('click',event=>{
+        //
+        //     let score_board = document.createElement('h3');
+        //     score_board.setAttribute('id','score_board');
+        //     score_board.textContent = 'Your Score is ' + count;
+        //     (quiz_game.parentNode).appendChild(score_board);
+        //     finish_game.style.display = 'none';
+        //
+        //
+        // });
+
+
+        let score_board = document.createElement('h3');
+        score_board.setAttribute('id','score_board');
+        score_board.textContent = 'Your Score is ' + count;
+        (quiz_game.parentNode).appendChild(score_board);
+        // finish_game.style.display = 'none';
+        quiz_game.style.display = 'none';
+
+        document.getElementById('restart_game').style.display = 'block';
+
+
+    }
+
+    // console.log(random);
+    // console.log(quiz_buffer);
+    let h1 = document.createElement('h1');
+    h1.textContent = quiz[random]["question"];
+    h1.setAttribute( "id", "quiz_question");
+    quiz_game.appendChild(h1);
+    let remove_button = document.querySelectorAll('#quiz_game button');
+    console.log("----------");
+    // console.log(remove_button);
+    // quiz_game.removeChild(remove_button);
+    for (let i = 0;i<remove_button.length;i++){
+
+        console.log(remove_button[i]);
+        quiz_game.removeChild(remove_button[i]);
+    }
+    let choose = ["A",'B',"C","D"];
+    for (let i = 0;i<4;i++){
+
+        let button = document.createElement("button");
+        button.textContent = quiz[random]["options"][i][choose[i]];
+        quiz_game.appendChild(button);
+        // console.log(button);
+        button.setAttribute('id','button'+i);
+    }
+
+
+
+
+    let option_button = document.querySelectorAll('#quiz_game button');
+
+    for(let i = 0;i<4;i++){
+        console.log(option_button[i]);
+        option_button[i].addEventListener('click',event=>{
+            console.log(random);
+            console.log(option_button[i].textContent);
+            console.log(quiz[random]["Answer"]);
+            if(option_button[i].textContent === quiz[random]["Answer"]){
+                console.log("it's correct !!!");
+                option_button[i].style.backgroundColor = '#2ecc71';
+                count ++;
+                console.log(count);
+                option_button[i].disabled = true;
+                next_game.style.display = 'block';
+            }
+            else {
+
+                option_button[i].style.backgroundColor = 'red';
+                console.log(count);
+                next_game.style.display = 'block';
+
+            }
+        });
+    }
+
+    console.log("count ---->" + count);
+    console.log("question_count ---->" + question_number);
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
